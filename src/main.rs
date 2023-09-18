bfa1072f8d381593ecfc9556f6b96e86ea9a36da
tathienbao <tathienbao.ttb@gmail.com>
2023-09-18 23:50:41 +0300

    define line turning right

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use smart_road::intersection_renderer::draw_north_right;
+use smart_road::intersection_renderer::draw_west_right;
+use smart_road::intersection_renderer::draw_south_right;
+use smart_road::intersection_renderer::draw_east_right;
 pub use sdl2::render::Canvas;
 
 fn main() {
@@ -12,10 +15,30 @@ fn main() {
 
     let mut canvas = window.into_canvas().build().unwrap();
 
-    // Draw the north right intersection.
-    draw_north_right(&mut canvas);
+    let mut event_pump = sdl_context.event_pump().unwrap();
 
-    // Assuming you also want to present the canvas and keep the window open for some time:
-    canvas.present();
-    std::thread::sleep(std::time::Duration::from_secs(5));
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            use sdl2::event::Event;
+            match event {
+                Event::Quit { .. } => break 'running,
+                _ => {},
+            }
+        }
+
+        canvas.set_draw_color(sdl2::pixels::Color::RGB(0, 0, 0));  // Set the canvas color to black
+        canvas.clear();  // Clear the canvas
+        
+        // Draw the north right intersection.
+        draw_north_right(&mut canvas);
+        draw_west_right(&mut canvas);
+        draw_south_right(&mut canvas);
+        draw_east_right(&mut canvas);
+
+        // Assuming you also want to present the canvas and keep the window open for some time:
+        canvas.present();
+
+        // Optional: If you want to limit the frame rate
+        std::thread::sleep(std::time::Duration::from_millis(100));
+    }
 }
--- src/render/intersection_renderer.rs
+++ src/render/intersection_renderer.rs
@@ -1,26 +1,109 @@
 pub use sdl2::gfx::primitives::DrawRenderer;
 pub use sdl2::rect::Point;
 
-const WINDOW_WIDTH: i32 = 800; // ví dụ, bạn có thể thay đổi giá trị này theo ý muốn
-const WINDOW_HEIGHT: i32 = 600; // ví dụ, bạn có thể thay đổi giá trị này theo ý muốn
-const CURVE_RADIUS_RIGHT: i32 = 50; // ví dụ, bạn có thể thay đổi giá trị này theo ý muốn
+const WINDOW_WIDTH: i32 = 800;
+const WINDOW_HEIGHT: i32 = 600;
+const CURVE_RADIUS_RIGHT: i32 = 50;
 
 pub fn draw_north_right(renderer: &mut sdl2::render::Canvas<sdl2::video::Window>) {
-    let start_point = Point::new(WINDOW_WIDTH / 2, 0);
-    let curve_point = Point::new(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 - CURVE_RADIUS_RIGHT as i32);
+    // Calculate the curve's center point.
+    let curve_center = Point::new(
+        WINDOW_WIDTH / 2 + CURVE_RADIUS_RIGHT,
+        WINDOW_HEIGHT / 2 + CURVE_RADIUS_RIGHT,
+    );
+
+    // Define start, curve, and end points
+    let start_point = Point::new(WINDOW_WIDTH / 2, WINDOW_HEIGHT);
+    let curve_start_point = Point::new(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 + CURVE_RADIUS_RIGHT);
+    let curve_end_point = Point::new(WINDOW_WIDTH / 2 + CURVE_RADIUS_RIGHT, WINDOW_HEIGHT / 2);
     let end_point = Point::new(WINDOW_WIDTH, WINDOW_HEIGHT / 2);
 
-    renderer.draw_line(start_point, curve_point).unwrap();
+    // Draw the straight line from the bottom to the curve's starting point.
+    renderer.set_draw_color(sdl2::pixels::Color::RGB(255, 0, 0));
+    renderer.draw_line(start_point, curve_start_point).unwrap();
 
-    // Sử dụng hàm arc
+    // Draw the curve (arc)
     renderer.arc(
-        WINDOW_WIDTH as i16 / 2, 
-        (WINDOW_HEIGHT / 2 - CURVE_RADIUS_RIGHT) as i16, 
-        CURVE_RADIUS_RIGHT as i16, 
-        0, 
-        90, // SDL2 uses degrees for arcs.
-        sdl2::pixels::Color::RGB(255, 0, 0)
+        curve_center.x() as i16,
+        curve_center.y() as i16,
+        CURVE_RADIUS_RIGHT as i16,
+        180, // Start from the bottom
+        270, // End at the left
+        sdl2::pixels::Color::RGB(255, 0, 0),
     ).unwrap();
 
-    renderer.draw_line(curve_point, end_point).unwrap();
+    // Draw the straight line from the curve's end point to the screen's right edge.
+    renderer.draw_line(curve_end_point, end_point).unwrap();
+}
+
+pub fn draw_east_right(renderer: &mut sdl2::render::Canvas<sdl2::video::Window>) {
+    let curve_center = Point::new(
+        WINDOW_WIDTH / 2 - CURVE_RADIUS_RIGHT,
+        WINDOW_HEIGHT / 2 + CURVE_RADIUS_RIGHT,
+    );
+
+    let start_point = Point::new(0, WINDOW_HEIGHT / 2);
+    let curve_start_point = Point::new(WINDOW_WIDTH / 2 - CURVE_RADIUS_RIGHT, WINDOW_HEIGHT / 2);
+    let curve_end_point = Point::new(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 + CURVE_RADIUS_RIGHT);
+    let end_point = Point::new(WINDOW_WIDTH / 2, WINDOW_HEIGHT);
+
+    renderer.set_draw_color(sdl2::pixels::Color::RGB(255, 0, 0));
+    renderer.draw_line(start_point, curve_start_point).unwrap();
+    renderer.arc(
+        curve_center.x() as i16,
+        curve_center.y() as i16,
+        CURVE_RADIUS_RIGHT as i16,
+        270,
+        0,
+        sdl2::pixels::Color::RGB(255, 0, 0),
+    ).unwrap();
+    renderer.draw_line(curve_end_point, end_point).unwrap();
+}
+
+pub fn draw_west_right(renderer: &mut sdl2::render::Canvas<sdl2::video::Window>) {
+    let curve_center = Point::new(
+        WINDOW_WIDTH / 2 + CURVE_RADIUS_RIGHT,
+        WINDOW_HEIGHT / 2 - CURVE_RADIUS_RIGHT,
+    );
+
+    let start_point = Point::new(WINDOW_WIDTH, WINDOW_HEIGHT / 2);
+    let curve_start_point = Point::new(WINDOW_WIDTH / 2 + CURVE_RADIUS_RIGHT, WINDOW_HEIGHT / 2);
+    let curve_end_point = Point::new(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 - CURVE_RADIUS_RIGHT);
+    let end_point = Point::new(WINDOW_WIDTH / 2, 0);
+
+    renderer.set_draw_color(sdl2::pixels::Color::RGB(255, 0, 0));
+    renderer.draw_line(start_point, curve_start_point).unwrap();
+    renderer.arc(
+        curve_center.x() as i16,
+        curve_center.y() as i16,
+        CURVE_RADIUS_RIGHT as i16,
+        90,  // Start from the top
+        180,    // End at the right
+        sdl2::pixels::Color::RGB(255, 0, 0),
+    ).unwrap();
+    renderer.draw_line(curve_end_point, end_point).unwrap();
+}
+
+pub fn draw_south_right(renderer: &mut sdl2::render::Canvas<sdl2::video::Window>) {
+    let curve_center = Point::new(
+        WINDOW_WIDTH / 2 - CURVE_RADIUS_RIGHT,
+        WINDOW_HEIGHT / 2 - CURVE_RADIUS_RIGHT,
+    );
+
+    let start_point = Point::new(WINDOW_WIDTH / 2, WINDOW_HEIGHT);
+    let curve_start_point = Point::new(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 + CURVE_RADIUS_RIGHT);
+    let curve_end_point = Point::new(WINDOW_WIDTH / 2 - CURVE_RADIUS_RIGHT, WINDOW_HEIGHT / 2);
+    let end_point = Point::new(0, WINDOW_HEIGHT / 2);
+
+    renderer.set_draw_color(sdl2::pixels::Color::RGB(255, 0, 0));
+    renderer.draw_line(start_point, curve_start_point).unwrap();
+    renderer.arc(
+        curve_center.x() as i16,
+        curve_center.y() as i16,
+        CURVE_RADIUS_RIGHT as i16,
+        0,    // Start from the right
+        90,   // End at the bottom
+        sdl2::pixels::Color::RGB(255, 0, 0),
+    ).unwrap();
+    renderer.draw_line(curve_end_point, end_point).unwrap();
 }
